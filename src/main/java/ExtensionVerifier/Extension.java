86b2369a6a5cdd4e31bf7ea16d9aef698b70e3ac
Adam <adamniemiec862@gmail.com>
2021-07-18 16:29:57 +0200

    new extensions added

--- src/main/java/ExtensionVerifier/Extension.java
+++ src/main/java/ExtensionVerifier/Extension.java
@@ -3,6 +3,11 @@ package ExtensionVerifier;
 public enum Extension {
     GIF("47494638"),
     JPG("FFD8FF"),
+    PDF("255044462D"),
+    PNG("89504E470D0A1A0A"),
+    DOC("D0CF11E0A1B11AE1"),
+    DOCX("504B0304"),
+    HTML("68746D6C3E"),
     TXT("text");
 
     private final String value;
@@ -20,6 +25,11 @@ public enum Extension {
         return switch (this) {
             case GIF -> "gif";
             case JPG -> "jpg";
+            case PDF -> "pdf";
+            case PNG -> "png";
+            case DOC -> "doc";
+            case DOCX -> "docx";
+            case HTML -> "html";
             case TXT -> "txt";
         };
     }
